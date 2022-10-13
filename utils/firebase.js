0e93edb30f7879e7a78b8072be0b52c6b69a1b72
ajayreddyg <ajayreddy13102000@gmail.com>
2022-10-13 15:07:51 +0530

    jscskc

--- utils/firebase.js
+++ utils/firebase.js
@@ -6,15 +6,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+  apiKey: "AIzaSyAMgOWgNm4RnZgAJlZaqoc220t8EancdHo",
+  authDomain: "auth-1b7fc.firebaseapp.com",
+  projectId: "auth-1b7fc",
+  storageBucket: "auth-1b7fc.appspot.com",
+  messagingSenderId: "376491722875",
+  appId: "1:376491722875:web:22e6ea0a3ebed4e7d1bade",
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
